b8ffb939e9db89bf13461ea2ae8696c5f3f98975
rickyzanna02 <ricky.zannoni@gmail.com>
2026-05-15 12:37:19 +0200

    Feature: Create whiteboard and marker. TODO: manage grab interaction

---
+++ Assets/Whiteboard/Scripts/Whiteboard.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Whiteboard : MonoBehaviour
+{
+    public Texture2D texture;
+    public Vector2 textureSize =  new Vector2(2048,2048);
+
+    
+    void Start()
+    {
+        var r = GetComponent<Renderer>();
+        texture = new Texture2D((int)textureSize.x, (int)textureSize.y);
+        r.material.mainTexture = texture;
+
+    }
+}
